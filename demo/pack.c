094c530c443934b49b95b0d1ed0e0c82c79c44ba
HkingAuditore <178854663@qq.com>
2019-10-03 17:23:06 +0800

    贪心算法 -2019-10-3 17:22:58

--- demo/pack.c
+++ demo/pack.c
@@ -13,6 +13,8 @@ typedef struct pack{
 	int MaxWeight;
 	int	NumOfItems;
 	Item* Picked;
+	int NumOfPicked;
+	int weightOfPicked;
 
 }Pack;
 
@@ -23,11 +25,13 @@ int GeneratePack(Pack* _pack, int _maxWeight, int _numOfItem)
 	// _pack->List = (int*)malloc(_numOfItem * sizeof(int));
 	for(int i=0;i< _numOfItem;i++)
 	{
-		_pack->Items[i].Weight = GetPowerNumber(_maxWeight);
+		_pack->Items[i].Weight = GetPowerNumber(_maxWeight-1)+1;
 		_pack->Items[i].Num = i;
 	}
 	_pack->NumOfItems = _numOfItem;
 	_pack->MaxWeight = _maxWeight;
+	_pack->NumOfPicked = 0;
+	_pack->weightOfPicked = 0;
 }
 
 int SoftPack(Pack* _pack)
@@ -52,18 +56,68 @@ int SoftPack(Pack* _pack)
 int PickItem(Pack* _pack)
 {
 	_pack->Picked = (Item*)malloc(_pack->NumOfItems * sizeof(Item));
+	int tempMax=0;
+	Item pickedItem[10];
+	int numOfPicked=0;
 	for(int i=0;i<_pack->NumOfItems;i++)
 	{
-		
+		int pickedWeight = _pack->Items[i].Weight;
+		int curPick=i+1;
+		Item tempPickedItem[10] = {_pack->Items[i]};
+		int tempNumOfPicked = 1;
+		//进入时如果没有超重，就往里面加东西
+		while (curPick<_pack->NumOfItems)
+		{
+			if(pickedWeight > _pack->MaxWeight)
+			{
+				//超重，减去最近加进去的物品
+				pickedWeight -= _pack->Items[curPick-1].Weight;
+				tempNumOfPicked--;
+				curPick++;
+				// break;
+			}
+			pickedWeight += _pack->Items[curPick].Weight;
+			tempPickedItem[tempNumOfPicked++] = _pack->Items[curPick++];
+		}
+		if (pickedWeight > _pack->MaxWeight)
+		{
+			//超重，减去最近加进去的物品
+			pickedWeight -= _pack->Items[--curPick].Weight;
+			tempNumOfPicked--;
+		}
+		if(pickedWeight>tempMax)
+		{
+			tempMax = pickedWeight;
+			numOfPicked = tempNumOfPicked;
+			for(int i=0;i< numOfPicked;i++)
+			{
+				_pack->Picked[i] = tempPickedItem[i];
+			}
+		}
 	}
+	_pack->NumOfPicked = numOfPicked;
+	for (int i = 0; i < _pack->NumOfPicked; i++)
+	{
+		pickedItem[i] = pickedItem[i];
+	}
+	_pack->weightOfPicked = tempMax;
 }
 
 void ShowPack(Pack _pack)
 {
 	for(int i=0;i<_pack.NumOfItems;i++)
 	{
-		printf("[%d]:%d\n", _pack.Items[i].Num, _pack.Items[i]);
+		printf("[%d]:%d\n", _pack.Items[i].Num, _pack.Items[i].Weight);
 	}
+}
+
+void ShowPicked(Pack _pack)
+{
+	for (int i = 0; i < _pack.NumOfPicked; i++)
+	{
+		printf("[%d]:%d\n", _pack.Picked[i].Num, _pack.Picked[i].Weight);
+	}
+	printf("Total weight:%d\n", _pack.weightOfPicked);
 }
 
 int main()
@@ -74,6 +128,8 @@ int main()
 	printf("\n");
 	SoftPack(&pack);
 	ShowPack(pack);
-
+	PickItem(&pack);
+	printf("\n");
+	ShowPicked(pack);
 	
 }
