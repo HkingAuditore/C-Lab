ff6466d038a9842e8c346c0bd44294a5e20f449c
HkingAuditore <178854663@qq.com>
2019-10-05 23:19:49 +0800

    增加对CLion的支持 -2019-10-5 23:19:36

---
+++ cmake-build-debug-mingw/CMakeFiles/C-Lab.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "C-Lab.exe"
+  "C-Lab.exe.manifest"
+  "C-Lab.pdb"
+  "CMakeFiles/C-Lab.dir/demo/pack.c.obj"
+  "libC-Lab.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C-Lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
