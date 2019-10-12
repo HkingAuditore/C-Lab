dc5884d0d3e544715f060a9aea869b732e9f95c1
HkingAuditore <178854663@qq.com>
2019-10-12 17:32:55 +0800

    完善KMP -2019-10-12 17:32:49

--- demo/2019-10-12 String/String.h
+++ demo/2019-10-12 String/String.h
@@ -9,35 +9,51 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct string {
+typedef struct string
+{
     char *Data;
     int Length;
     int *Info;
 } String;
 
-String *GenerateString(char *_data) {
-//    printf("IN!");
-    String *str = (String *) malloc(sizeof(String));
-    str->Data = (char *) malloc(sizeof(char) * (strlen(_data)+1));
-    for (int i = 0; i < (int) strlen(_data); ++i) {
+String *GenerateString(char *_data)
+{
+    // printf("IN generate!\n");
+    String *str = (String *)calloc(1, sizeof(String));
+    // printf("IN generate!*2\n");
+    str->Data = (char *)calloc((strlen(_data) + 10), sizeof(char));
+    // printf("IN generate!*3\n");
+    for (int i = 0; i < (int)strlen(_data); ++i)
+    {
+        // printf("now put %c\n", _data[i]);
         str->Data[i] = _data[i];
     }
-    str->Data[strlen(_data)]='\0';
-    str->Length=strlen(_data);
-//    printf("OUT!");
+    str->Data[strlen(_data) + 1] = '\0';
+    str->Length = (int)strlen(_data);
+    //    printf("OUT!");
     return str;
 }
 
-int WriteString(String _string) {
-//    printf("In write!\n");
-    puts(_string.Data);
+int WriteString(String _str)
+{
+    //    printf("In write!\n");
+    puts(_str.Data);
 }
 
-int WriteInfo(String _string) {
-    for (int i = 0; i < _string.Length; ++i) {
-        printf("%d ", _string.Info[i]);
+int WriteInfo(String _str)
+{
+    for (int i = 0; i < _str.Length; ++i)
+    {
+        printf("%d ", _str.Info[i]);
     }
     printf("\n");
 }
 
+int DestroyString(String *_str)
+{
+    free(_str->Data);
+    free(_str->Info);
+    free(_str);
+    return 0;
+}
 #endif //C_LAB_STRING_H
